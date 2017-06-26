afbea46f3873b3b9cc3e67460dfcb41739c2d695
wmszhe <wmszhe@gmail.com>
2017-06-26 23:54:24 +0800

    hello world

--- firstDemo/firstDemo/urls.py
+++ firstDemo/firstDemo/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import url
+
+from . import view
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', view.hello),
 ]
